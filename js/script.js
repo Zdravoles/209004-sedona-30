aad7f88ff2975c873d09bfc320ffe822d83566f8
Zdravoles <71260072+Zdravoles@users.noreply.github.com>
2021-03-16 18:52:38 +0300

    Правки от наставника. Проверил Стайлгайд

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const objFormClass = document.getElementById('popup_form');
+const btnSearch = document.querySelector('.search-link');
 function popup_form() {
+	objFormClass.preventDefault;
 	if (objFormClass.className=='form-hidden') {
 		objFormClass.className='form-show';
 	}
@@ -7,6 +9,10 @@ function popup_form() {
 		objFormClass.className='form-hidden';
 	}
 }
+btnSearch.onclick = function (event) {
+	event.preventDefault();
+  	popup_form();
+};
 window.onload = popup_form;
 window.addEventListener("keydown", function (event) {
   	if (event.keyCode === 27) {
