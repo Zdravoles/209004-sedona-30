882225dcf92f05db8e81f6555a2d0cc7c35312ca
Zdravoles <71260072+Zdravoles@users.noreply.github.com>
2021-03-17 16:13:18 +0300

    добавил анимацию на ошибку в форме и выезд формы

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-const objFormClass = document.getElementById('popup_form');
+const objForm = document.getElementById('popup_form');
 const btnSearch = document.querySelector('.search-link');
+const dateIn = objForm.querySelector('[name=dateIn]');
+
 function popup_form() {
-	if (objFormClass.className=='form-hidden') {
-		objFormClass.className='form-show';
+	dateIn.classList.remove('order-error');
+	if (objForm.className=='form-hidden') {
+		objForm.className='form-show';
 	}
 	else {
-		objFormClass.className='form-hidden';
+		objForm.className='form-hidden';
 	}
 }
 btnSearch.onclick = function (event) {
 	event.preventDefault();
   	popup_form();
 };
+
 window.onload = popup_form;
-window.addEventListener("keydown", function (event) {
+
+window.addEventListener('keydown', function (event) {
   	if (event.keyCode === 27) {
 	    popup_form();
+	}
+});
+
+objForm.addEventListener('submit', function (event) {
+	if (!dateIn.value) {
+		dateIn.classList.add('order-error');
+		event.preventDefault();
 	}
 });
